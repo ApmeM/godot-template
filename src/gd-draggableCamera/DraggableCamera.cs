88ccb536bb7abd81223a2388d9e0a141acd25ce0
Artem Votintsev <artem.votincev@gmail.com>
2022-12-03 10:01:31 +0100

    Add draggable camera and floating text features

---
+++ src/gd-draggableCamera/DraggableCamera.cs
@@ -0,0 +1,155 @@
+using Godot;
+using GodotAnalysers;
+using System;
+
+[SceneReference("DraggableCamera.tscn")]
+public partial class DraggableCamera
+{
+    public override void _Ready()
+    {
+        base._Ready();
+        this.FillMembers();
+    }
+
+    private bool drag = false;
+    private Vector2 initPosMouse = Vector2.Zero;
+    
+    [Export]
+    public float minimumZoom = 0.3f;
+    
+    [Export]
+    public float maximumZoom = 3f;
+
+    [Export]
+    public bool enabled = true;
+    /// <summary>
+    ///     The zoom value should be between -1 and 1. 
+    ///     This value is then translated to be from minimumZoom to maximumZoom.
+    ///     This lets you set appropriate minimum/maximum values then use a more intuitive -1 to 1 mapping to change the zoom.
+    /// </summary>
+    public float NormalizedZoom
+    {
+        get
+        {
+            if (this.Zoom.x == 1)
+                return 0f;
+
+            if (this.Zoom.x < 1)
+                return Map(this.Zoom.x, this.minimumZoom, 1, -1, 0);
+            return Map(this.Zoom.x, 1, this.maximumZoom, 0, 1);
+        }
+        set
+        {
+            var newZoom = Mathf.Clamp(value, -1, 1);
+            if (newZoom == 0)
+                this.Zoom = Vector2.One;
+            else if (newZoom < 0)
+                this.Zoom = Vector2.One * Map(newZoom, -1, 0, this.minimumZoom, 1);
+            else
+                this.Zoom = Vector2.One * Map(newZoom, 0, 1, 1, this.maximumZoom);
+        }
+    }
+    
+    /// <summary>
+    ///     Minimum non-scaled value (0 - float.Max) that the camera zoom can be. 
+    ///     Defaults to 0.3
+    /// </summary>
+    public float MinimumZoom
+    {
+        get => this.minimumZoom;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new Exception("MinimumZoom must be greater then zero.");
+            }
+
+            if (this.Zoom.x < value)
+                this.Zoom = Vector2.One * value;
+
+            this.minimumZoom = value;
+        }
+    }
+
+    /// <summary>
+    ///     maximum non-scaled value (0 - float.Max) that the camera zoom can be. Defaults to 3
+    /// </summary>
+    /// <value>The maximum zoom.</value>
+    public float MaximumZoom
+    {
+        get => this.maximumZoom;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new Exception("MaximumZoom must be greater then zero.");
+            }
+
+            if (this.Zoom.x > value)
+                this.Zoom = Vector2.One * value;
+
+            this.maximumZoom = value;
+        }
+    }
+
+    public override void _Process(float delta)
+    {
+        base._Process(delta);
+
+        if (!enabled)
+        {
+            return;
+        }
+
+        if (Input.IsMouseButtonPressed(1))
+        {
+            if (drag)
+            {
+                var mousePos = this.GetViewport().GetMousePosition();
+                this.GlobalPosition += (initPosMouse - mousePos) * this.Zoom;
+                this.initPosMouse = mousePos;
+            }
+            else
+            {
+                if (initPosMouse == Vector2.Zero)
+                {
+                    this.initPosMouse = this.GetViewport().GetMousePosition();
+                }
+
+                this.drag = this.initPosMouse != this.GetViewport().GetMousePosition();
+            }
+        }
+        else
+        {
+            this.drag = false;
+            this.initPosMouse = Vector2.Zero;
+        }
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+
+        if (!enabled)
+        {
+            return;
+        }
+
+        if (@event is InputEventMouseButton buttonEvent)
+        {
+            if (buttonEvent.ButtonIndex == (int)ButtonList.WheelUp)
+            {
+                this.NormalizedZoom += 0.1f;
+            }
+            if (buttonEvent.ButtonIndex == (int)ButtonList.WheelDown)
+            {
+                this.NormalizedZoom -= 0.1f;
+            }
+        }
+    }
+
+    private static float Map(float value, float leftMin, float leftMax, float rightMin, float rightMax)
+    {
+        return rightMin + (value - leftMin) * (rightMax - rightMin) / (leftMax - leftMin);
+    }
+}
---
+++ src/gd-floatingText/FloatingText.cs
@@ -0,0 +1,33 @@
+using Godot;
+using GodotAnalysers;
+
+[SceneReference("FloatingText.tscn")]
+public partial class FloatingText
+{
+    public override void _Ready()
+    {
+        base._Ready();
+        this.FillMembers();
+    }
+
+    public async void ShowValue(string value, Vector2 direction, float duration, float spread, Color? color = null, bool highlite = false)
+    {
+        this.Modulate = color ?? new Color(1, 1, 1);
+
+        this.Text = value;
+        var movement = direction.Rotated((float)GD.RandRange(-spread / 2, spread / 2));
+        this.RectPivotOffset = this.RectSize / 2;
+
+        tween.InterpolateProperty(this, "rect_position", this.RectPosition, this.RectPosition + movement, duration, Tween.TransitionType.Linear, Tween.EaseType.InOut);
+        tween.InterpolateProperty(this, "modulate:a", 1.0, 0.0, duration, Tween.TransitionType.Linear, Tween.EaseType.InOut);
+
+        if (highlite)
+        {
+            tween.InterpolateProperty(this, "rect_scale", this.RectScale * 2, this.RectScale, 0.4f, Tween.TransitionType.Back, Tween.EaseType.In);
+        }
+
+        tween.Start();
+        await ToSignal(tween, "tween_all_completed");
+        QueueFree();
+    }
+}
